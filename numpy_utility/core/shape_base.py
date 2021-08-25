4f9156f4507b64091910118b5a839c25d32aef61
Yugo Omura <yomura@YugonoMacBook-puro.local>
2021-08-25 20:27:07 +0900

    modified:   numpy_utility/core/shape_base.py 	modified:   tests/test_merge_arrays.py

--- numpy_utility/core/shape_base.py
+++ numpy_utility/core/shape_base.py
@@ -1,5 +1,6 @@
 import itertools
 import numpy as np
+import collections
 from . import fromnumeric as _fromnumeric_module
 
 
@@ -8,6 +9,8 @@ __all__ = ["merge_arrays"]
 
 def get_extended_dtype(type1, type2, *types):
     types = list(map(np.dtype, itertools.chain([type1, type2], types)))
+    assert all(len(type_.descr) == 1 for type_ in types)
+    assert all(len(type_.descr[0]) == 2 for type_ in types)
     assert all(types[0].kind == type_.kind for type_ in types[1:])
     assert all(types[0].byteorder == type_.byteorder for type_ in types[1:])
     kind = types[0].kind
@@ -23,30 +26,77 @@ def get_extended_dtype(type1, type2, *types):
         raise TypeError(kind)
 
 
+def take(a, idx):
+    if isinstance(idx, tuple):
+        if len(idx) == 0:
+            return a
+        else:
+            return take(a[idx[0]], idx[1:])
+    else:
+        return a[idx]
+
+
+def get_flatten_dtype_names(dtype: np.dtype):
+    return [
+        e
+        for name in dtype.names
+        for e in (
+            [(name,)]
+            if dtype[name].names is None else
+            ((name, *dtypes) for dtypes in get_flatten_dtype_names(dtype[name]))
+        )
+    ]
+
+
+def flatten_to_nested_dtype(flatten_dtypes: list):
+    def f(common_dtype_1st_name, matched_dtypes):
+        matched_dtypes = list(matched_dtypes)
+        n_depths = [len(matched_dtype[0]) for matched_dtype in matched_dtypes]
+        if len(n_depths) == 1 and n_depths[0] == 1:
+            return common_dtype_1st_name, *matched_dtypes[0][1:]
+        return (
+            common_dtype_1st_name,
+            flatten_to_nested_dtype([(names, dtype) for (_, *names), dtype in matched_dtypes])
+        )
+    return list(itertools.starmap(f, itertools.groupby(flatten_dtypes, lambda fd: fd[0][0])))
+
+
 def merge_arrays(arrays, validate_unique_columns=True):
     assert len(arrays) > 1
-    common_dtype_names = list(set(arrays[0].dtype.names).intersection(*(a.dtype.names for a in arrays[1:])))
+    common_dtype_names = list(
+        set(
+            get_flatten_dtype_names(arrays[0].dtype)
+        ).intersection(*(get_flatten_dtype_names(a.dtype) for a in arrays[1:]))
+    )
+    common_dtype_1st_names = [name_at_1dim for name_at_1dim, *_ in common_dtype_names]
 
-    common_dtypes = [
-        (common_dtype_name, get_extended_dtype(*(a.dtype[common_dtype_name] for a in arrays)))
+    common_dtypes = flatten_to_nested_dtype([
+        (common_dtype_name, get_extended_dtype(*(take(a.dtype, common_dtype_name) for a in arrays)))
         for common_dtype_name in common_dtype_names
-    ]
+    ])
 
     assert len(common_dtypes) > 0
-    # assert all(common_dtypes[0] == dtypes for dtypes in common_dtypes[1:])  # 全てのaが共通のdtypeを持つ
-    # common_dtypes = common_dtypes[0]
-    # common_dtype_names = [name for name, *_ in common_dtypes]
-    arrays_dtypes = [
-        [d for d in a.dtype.descr if d[0] not in common_dtype_names]
+    other_dtype_names = [
+        [dtype_name for dtype_name in get_flatten_dtype_names(a.dtype) if dtype_name[0] not in common_dtype_1st_names]
         for a in arrays
     ]
-    arrays_dtype_names = [[name for name, *_ in dtype_descr] for dtype_descr in arrays_dtypes]
-    other_dtypes = [dtype for arrays_dtype in arrays_dtypes for dtype in arrays_dtype]
-    arrays_values_at_common_dtype = [np.unique(a[common_dtype_names].astype(common_dtypes, copy=False)) for a in arrays]
+    other_dtypes = flatten_to_nested_dtype([
+        (dtype_name, *take(a.dtype, dtype_name).descr[0][1:])
+        for a, dtype_names in zip(arrays, other_dtype_names)
+        for dtype_name in dtype_names
+    ])
+    # other_dtypes = [dtype for arrays_dtype in other_dtypes for dtype in arrays_dtype]
+
+    arrays_values_at_common_dtype = [
+        np.unique(a[common_dtype_1st_names].astype(common_dtypes, copy=False))
+        for a in arrays
+    ]
 
     if validate_unique_columns:
-        if not all(shape_v == shape_a
-                   for shape_v, shape_a in zip(map(np.shape, arrays_values_at_common_dtype), map(np.shape, arrays))):
+        if not all(
+            shape_v == shape_a
+            for shape_v, shape_a in zip(map(np.shape, arrays_values_at_common_dtype), map(np.shape, arrays))
+        ):
             i = next(
                 i
                 for i, (shape_v, shape_a) in enumerate(
@@ -56,13 +106,19 @@ def merge_arrays(arrays, validate_unique_columns=True):
             )
             raise ValueError(f"Non-unique values found in arrays[{i}][{common_dtype_names}]")
 
-    values_at_common_dtype = np.unique([v for values in arrays_values_at_common_dtype for v in values])
+    values_at_common_dtype = np.unique([
+        v for values in arrays_values_at_common_dtype for v in values
+    ]).astype(common_dtypes, copy=False)
 
     a = np.ma.empty(len(values_at_common_dtype), dtype=[*common_dtypes, *other_dtypes])
     a.mask = True
 
-    for i, dtype_names in enumerate(arrays_dtype_names):
-        indices = _fromnumeric_module.search_matched(values_at_common_dtype, arrays[i][common_dtype_names])
-        for name in arrays[i].dtype.names:
-            a[name][indices] = arrays[i][name]
+    for name in common_dtype_1st_names:
+        a[name] = values_at_common_dtype[name]
+
+    for i, dtype_names in enumerate(other_dtype_names):
+        indices = _fromnumeric_module.search_matched(values_at_common_dtype, arrays[i][common_dtype_1st_names])
+        for name in dtype_names:
+            a_view = take(a, name)
+            a_view[indices] = take(arrays[i], name)
     return a
--- tests/test_merge_arrays.py
+++ tests/test_merge_arrays.py
@@ -4,21 +4,40 @@ import numpy_utility as npu
 
 
 class MyTestCase(unittest.TestCase):
-    def test1(self):
+    def test_basic1(self):
         a1 = np.empty((0,), dtype=[("index", "i4"), ("b", "f8")])
         a2 = np.empty((0,), dtype=[("index", "i4"), ("c", "f8")])
         npu.merge_arrays([a1, a2])
 
-    def test2(self):
+    def test_basic2(self):
         a1 = np.empty((10,), dtype=[("index", "U10"), ("b", "f8")])
         a2 = np.empty((10,), dtype=[("index", "U2"), ("c", "f8")])
         a1["index"] = np.arange(10)
         a2["index"] = np.arange(10)
         npu.merge_arrays([a1, a2])
 
-    def test3(self):
+    def test_basic3(self):
         a1 = np.empty((0,), dtype=[("index", "i4"), ("b", "f8")])
         a2 = np.empty((10,), dtype=[("index", "i8"), ("c", "f8")])
+        a2["index"] = np.arange(10)
+        npu.merge_arrays([a1, a2])
+
+    def test_nested1(self):
+        a1 = np.empty((10,), dtype=[("index", "U10"), ("parent", [("b", "f8")])])
+        a2 = np.empty((10,), dtype=[("index", "U2"), ("parent", [("c", "f8")])])
+        a1["index"] = np.arange(10)
+        a2["index"] = np.arange(10)[::-1]
+        a1["parent"]["b"] = np.arange(10)
+        a2["parent"]["c"] = np.arange(10)[::-1]
+        npu.merge_arrays([a1, a2])
+
+    def test_nested2(self):
+        a1 = np.empty((10,), dtype=[("index", "U10"), ("parent", [("b", "f8"), ("c", "f8")])])
+        a2 = np.empty((10,), dtype=[("index", "U2"), ("parent", [("d", "f8"), ("e", "f8"), ("f", "i8")])])
+        a1["index"] = np.arange(10)
+        a2["index"] = np.arange(10)[::-1]
+        a1["parent"]["b"] = np.arange(10)
+        a2["parent"]["d"] = np.arange(10)[::-1]
         npu.merge_arrays([a1, a2])
 
 
